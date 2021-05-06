524f036cc09b5de2dd49464f1c9ec9b7bdc06b57
LithTheDev <LithTheDev@Gmail.com>
2021-05-06 08:33:08 -0400

    reorder images

--- src/app/image-container/image-container.component.ts
+++ src/app/image-container/image-container.component.ts
@@ -14,6 +14,22 @@ export class ImageContainerComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  shiftUp(image: number) {
+    if (image === 0) return
+    let images = this.events.images;
+    const imageToShift = images.splice(image, 1)[0]
+    images.splice(image - 1, 0, imageToShift)
+    this.events.images = images
+  }
+
+  shiftDown(image: number) {
+    if (image === this.events.images.length - 1) return
+    let images = this.events.images;
+    const imageToShift = images.splice(image, 1)[0]
+    images.splice(image + 1, 0, imageToShift)
+    this.events.images = images
+  }
+
   recordDeath(image: number, event: MouseEvent) {
     this.events.recordDeath(image, event.offsetX, event.offsetY)
     console.log(this.events.events)
